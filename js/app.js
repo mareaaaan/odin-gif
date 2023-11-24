0e8d6429153a2f8ec018b740cc613d68e6af76dc
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-11-24 18:35:53 +0000

    Refactor using async/await

--- js/app.js
+++ js/app.js
@@ -3,17 +3,18 @@ const search = document.querySelector(".search");
 
 const API_KEY = "d6SSC6oW7giniXQVL39GsbgYHBuXKl2u";
 
-function fetchGif() {
-	fetch(
-		`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${search.value}`,
-		{
-			mode: "cors",
-		},
-	)
-		.then((response) => response.json())
-		.then((response) => {
-			img.src = response.data.images.original.url;
-		});
+async function fetchGif() {
+	if (search.value) {
+		const response = await fetch(
+			`https://api.giphy.com/v1/gifs/translate?api_key=${API_KEY}&s=${search.value}`,
+			{
+				mode: "cors",
+			},
+		);
+		const parsedResponse = await response.json();
+
+		img.src = parsedResponse.data.images.original.url;
+	}
 }
 
 search.addEventListener("change", fetchGif);
